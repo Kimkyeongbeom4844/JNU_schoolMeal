95a7b3362cba85c2b3d83ef8d0cff9ab93aa135d
kimkyeongbeom <bibix321@naver.com>
2022-11-18 17:38:55 +0900

    hide URL

--- router/html.js
+++ router/html.js
@@ -4,9 +4,7 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 router.route("/").get((req, res) => {
-  const url =
-    process.env.EXPRESS_APP_MEALADDRESS ||
-    "https://www.jejunu.ac.kr/camp/stud/foodmenu";
+  const url = process.env.EXPRESS_APP_MEALADDRESS;
   request(url, (err, result, html) => {
     const $ = cheerio.load(html);
     return res.json($("table.border_left").text());
