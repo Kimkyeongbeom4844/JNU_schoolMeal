cf8410d77dfcd1bb59216882f8b3e8a5b3ca044e
kimkyeongbeom <bibix321@naver.com>
2022-11-22 09:41:24 +0900

    add getMenu

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
 export const getMenu = () =>
-  fetch(process.env.REACT_APP_API || "http://127.0.0.1:8080/html")
-    .then((res) => res.json())
-    .then((data) => data);
+  fetch(process.env.REACT_APP_API || "http://127.0.0.1:8080/html").then((res) =>
+    res.json()
+  );
