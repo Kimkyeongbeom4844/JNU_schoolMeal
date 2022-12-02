7ca32af9c6bb30f6ecbdae0dca269f9b814104f8
kimkyeongbeom <bibix321@naver.com>
2022-12-02 12:05:43 +0900

    fetch -> axios

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,19 +1,37 @@
+import axios from "axios";
+
 export const getMenu = () =>
   fetch(process.env.REACT_APP_API || "http://127.0.0.1:8080/html").then((res) =>
     res.json()
   );
 
 export const testApi = () =>
-  fetch("https://kingbeom.herokuapp.com/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: "1111@naver.com",
-      password: "1111",
-    }),
-    credentials: "include",
-  })
-    .then((res) => res.json())
+  axios
+    .post(
+      "https://kingbeom.herokuapp.com/login",
+      JSON.stringify({
+        email: "1111@naver.com",
+        password: "1111",
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      }
+    )
+    .then((res) => console.log(res))
     .catch((err) => console.log(err));
+// fetch("https://kingbeom.herokuapp.com/login", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     email: "1111@naver.com",
+//     password: "1111",
+//   }),
+//   credentials: "include",
+// })
+//   .then((res) => res.json())
+//   .catch((err) => console.log(err));
